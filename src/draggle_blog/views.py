b0dbb55b6313b77efb26040ef47620170f20dbe4
dipto0321 <diptokmk47@gmail.com>
2019-10-28 13:24:24 +0600

    Add base template
    - Solve DRY related issues

--- src/draggle_blog/views.py
+++ src/draggle_blog/views.py
@@ -7,4 +7,6 @@ def home_page(request):
 
 
 def hello_world(request):
-    return render(request, "hello_world.html")
+    focus_message = "Hey! This is Django"
+    title = "Hello! Django"
+    return render(request, "hello_world.html", {"title": title, "focus_message": focus_message})
