7846ac3b260722f8728d3aad0fa6fe1a74087432
dipto0321 <diptokmk47@gmail.com>
2019-10-28 12:38:34 +0600

    Add template
    - Added template directory
    - Add render funtion in viwes.py
    - Add materializecss

--- src/draggle_blog/urls.py
+++ src/draggle_blog/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
-from .views import home_page
+from .views import home_page, hello_world
 
 urlpatterns = [
     path('draggle-admin-dash/', admin.site.urls),
-    path('', home_page)
+    path('', home_page),
+    re_path(r'^hello?/$', hello_world)
 ]
--- src/draggle_blog/views.py
+++ src/draggle_blog/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home_page(request):
     return HttpResponse("<h1>Home Page View</h1>")
+
+
+def hello_world(request):
+    return render(request, "hello_world.html")
