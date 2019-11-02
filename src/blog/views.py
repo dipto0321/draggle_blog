148963b3d00ebb37cbd6b4ac8afc1d56599d09af
dipto0321 <diptokmk47@gmail.com>
2019-11-02 18:47:48 +0600

    Added static blog view get method

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 # Create your views here.
+def get_blog_post(request):
+    blog_obj = BlogPost.objects.get(id=1)
+    template_name = "blog_post_detail.html"
+    context = {"blog_obj": blog_obj, "title": "Blog Posts Detail"}
+    return render(request, template_name, context)
+
--- src/draggle_blog/urls.py
+++ src/draggle_blog/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, re_path
 
 from .views import home_page, hello_world
+from blog.views import get_blog_post
 
 urlpatterns = [
-    path('draggle-admin-dash/', admin.site.urls),
-    path('', home_page),
-    re_path(r'^hello?/$', hello_world)
+    path("draggle-admin-dash/", admin.site.urls),
+    path("", home_page),
+    re_path(r"^hello?/$", hello_world),
+    path("blog/posts", get_blog_post),
 ]
