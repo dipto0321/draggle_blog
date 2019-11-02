89c814c5d6f45464a9b28e906dd6954892fc7845
dipto0321 <diptokmk47@gmail.com>
2019-11-02 19:37:29 +0600

    Basic error handelling for blog_post

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
 
 # Create your views here.
-def get_blog_post(request):
-    blog_obj = BlogPost.objects.get(id=1)
-    template_name = "blog_post_detail.html"
+def get_blog_post(request, post_id):
+    blog_obj = get_object_or_404(BlogPost, id=post_id)
+    template_name = "blog_post.html"
     context = {"blog_obj": blog_obj, "title": "Blog Posts Detail"}
     return render(request, template_name, context)
 
