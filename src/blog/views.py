8587da9fc58e66e9e610c8206efd4a96c072ae15
dipto0321 <diptokmk47@gmail.com>
2019-11-07 15:36:53 +0600

    Outline blog post CRUD view with template
    - Generate five templates
    - Intial complete index view
    - Complete post retrive/detail view
    - Rearrange all blog post view template inside a folder

--- src/blog/views.py
+++ src/blog/views.py
@@ -10,15 +10,20 @@ def get_blog_post(request, slug):
 
 
 def blog_post_list_view(request):
-    pass
+    template_name = "blog_posts/index.html"
+    context = {"post_list": [], "title": "Post List"}
+    return render(request, template_name, context)
 
 
 def blog_post_create_view(request):
     pass
 
 
-def blog_post_retrive_view(request):
-    pass
+def blog_post_retrive_view(request, slug):
+    post_obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = "blog_posts/retrive.html"
+    context = {"post": post_obj}
+    return render(request, template_name, context)
 
 
 def blog_post_update_view(request):
--- src/draggle_blog/urls.py
+++ src/draggle_blog/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path
 
 from .views import home_page, about_page, contact_page
-from blog.views import get_blog_post
+from blog.views import blog_post_list_view, blog_post_retrive_view
 
 urlpatterns = [
     path("draggle-admin-dash/", admin.site.urls),
     path("", home_page),
     path("about/", about_page),
     path("contact/", contact_page),
-    path("blog/posts/<str:slug>/", get_blog_post),
+    path("blog/posts/", blog_post_list_view),
+    path("blog/posts/<str:slug>/", blog_post_retrive_view),
 ]
