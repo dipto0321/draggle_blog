8ee49a94f836adffda767635df47e42d1d75e02a
dipto0321 <diptokmk47@gmail.com>
2019-11-08 02:11:34 +0600

    Add ui for create and modify navbar
    - Add basic form view for create post
    - Add mobile sidenave feature for menus

--- src/draggle_blog/urls.py
+++ src/draggle_blog/urls.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from django.urls import path
 
 from .views import home_page, about_page, contact_page
-from blog.views import blog_post_list_view, blog_post_retrive_view
+from blog.views import (
+    blog_post_list_view,
+    blog_post_retrive_view,
+    blog_post_create_view,
+)
 
 urlpatterns = [
     path("draggle-admin-dash/", admin.site.urls),
@@ -10,5 +14,6 @@ urlpatterns = [
     path("about/", about_page),
     path("contact/", contact_page),
     path("blog/", blog_post_list_view),
+    path("blog/add_post/", blog_post_create_view),
     path("blog/<str:slug>/", blog_post_retrive_view),
 ]
