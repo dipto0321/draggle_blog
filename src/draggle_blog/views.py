29af79d212e8d64c296566e94ab9c1b632eb15ab
dipto0321 <diptokmk47@gmail.com>
2019-11-07 15:43:19 +0600

    Organize all templates in a folder

--- src/draggle_blog/views.py
+++ src/draggle_blog/views.py
@@ -3,18 +3,18 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    template_name = "home.html"
+    template_name = "static/home.html"
     context = {"title": "Home", "content": "Awesome Draggle Blog app!"}
     return render(request, template_name, context)
 
 
 def about_page(request):
-    template_name = "about.html"
+    template_name = "static/about.html"
     context = {"title": "About"}
     return render(request, template_name, context)
 
 
 def contact_page(request):
-    template_name = "contact.html"
+    template_name = "static/contact.html"
     context = {"title": "Contact"}
     return render(request, template_name, context)
