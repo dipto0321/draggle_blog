e2717ca831ff4feeadcd66a9b4d5bae49cc358ea
dipto0321 <diptokmk47@gmail.com>
2019-11-08 17:56:17 +0600

    Seperates blog related templates in their own app

--- src/blog/views.py
+++ src/blog/views.py
@@ -4,34 +4,34 @@ from .models import BlogPost
 
 def blog_post_list_view(request):
     post_list = BlogPost.objects.all()
-    template_name = "blog_posts/index.html"
+    template_name = "index.html"
     context = {"post_list": post_list, "title": "Post List"}
     return render(request, template_name, context)
 
 
 def blog_post_create_view(request):
-    template_name = "blog_posts/create.html"
+    template_name = "create.html"
     context = {"form": None, "title": "Post Create"}
     return render(request, template_name, context)
 
 
 def blog_post_retrive_view(request, slug):
     post_obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = "blog_posts/retrive.html"
+    template_name = "retrive.html"
     context = {"post": post_obj}
     return render(request, template_name, context)
 
 
 def blog_post_update_view(request, slug):
     post_obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = "blog_posts/update.html"
+    template_name = "update.html"
     context = {"form": None, "post_obj": post_obj}
     return render(request, template_name, context)
 
 
 def blog_post_delete_view(request, slug):
     post_obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = "blog_posts/delete.html"
+    template_name = "delete.html"
     context = {"post_obj": post_obj}
     return render(request, template_name, context)
 
