8afd22e47593a5ae73ea2bcad6f450cbee6f141e
dipto0321 <diptokmk47@gmail.com>
2019-11-08 17:53:57 +0600

    Seperate blog related urls in their own app

--- src/draggle_blog/urls.py
+++ src/draggle_blog/urls.py
@@ -1,19 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from .views import home_page, about_page, contact_page
-from blog.views import (
-    blog_post_list_view,
-    blog_post_retrive_view,
-    blog_post_create_view,
-)
+from blog import urls
 
 urlpatterns = [
     path("draggle-admin-dash/", admin.site.urls),
     path("", home_page),
     path("about/", about_page),
     path("contact/", contact_page),
-    path("blog/", blog_post_list_view),
-    path("blog/add_post/", blog_post_create_view),
-    path("blog/<str:slug>/", blog_post_retrive_view),
+    path("blog/", include(urls)),
 ]
