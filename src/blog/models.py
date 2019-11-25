7c2bc17e4a82d135e08e77f192460e53de7ac1a5
dipto0321 <diptokmk47@gmail.com>
2019-11-26 00:33:25 +0600

    Add user as a foreignkey

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,9 +1,13 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
 
+User = settings.AUTH_USER_MODEL
+
 
 class BlogPost(models.Model):
+    user = models.ForeignKey(User, null=True, default=1, on_delete=models.SET_NULL)
     title = models.CharField(max_length=150)
     slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)
