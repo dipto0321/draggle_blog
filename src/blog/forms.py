f6989b32563dcaf0314473a2625fd82b30dde873
dipto0321 <diptokmk47@gmail.com>
2019-11-28 19:57:46 +0600

    Complete basic update feature

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -13,7 +13,10 @@ class BlogPostModelForm(forms.ModelForm):
 
     def clean_title(self, *args, **kwargs):
         title = self.cleaned_data.get("title")
+        instance = self.instance
         qs = BlogPost.objects.filter(title__iexact=title)
+        if instance is not None:
+            qs = qs.exclude(pk=instance.pk)
         if qs.exists():
             raise forms.ValidationError("This title has been already taken")
         return title
